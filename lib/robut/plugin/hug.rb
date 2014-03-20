2fdc42492c86ab8c79069486078ceb872b6a6e6c
Manoj Dayaram <noj@moovweb.com>
2014-03-20 00:24:59 -0700

    added hugs

---
+++ lib/robut/plugin/hug.rb
@@ -0,0 +1,9 @@
+class Robut::Plugin::Hug
+  include Robut::Plugin
+
+  desc "hug - receive a virtual hug from nojbot."
+
+  match /\W+hugs?/, :sent_to_me => true do
+    reply("https://dl.dropboxusercontent.com/u/9921601/virtual-hug.gif")
+  end
+end
---
+++ spec/robut/plugin/hug_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'robut/plugin/hug'
+
+describe Robut::Plugin::Hug do
+  before do
+    @connection = Robut::ConnectionMock.new
+    @presence = Robut::PresenceMock.new(@connection)
+    @plugin = Robut::Plugin::Hug.new(@presence)
+  end
+
+  it "gives you a hug when you need it" do
+    @plugin.handle(Time.now, "noj", "@robut I need a hug...")
+    @plugin.reply_to.replies.should eql(["https://dl.dropboxusercontent.com/u/9921601/virtual-hug.gif"])
+  end
+end
