1388c63bbba0303a86e3a47f609cddf752adb405
Manoj Dayaram <noj@moovweb.com>
2014-03-21 15:45:56 -0700

    changing hamlet wisdom to text that I've written to mimic me more

--- lib/nojbot/wisdom.rb
+++ lib/nojbot/wisdom.rb
@@ -2,7 +2,7 @@ require 'marky_markov'
 module NojBot
   class Wisdom
     @@markov = MarkyMarkov::TemporaryDictionary.new
-    @@markov.parse_file "hamlet.txt"
+    @@markov.parse_file "noj.txt"
 
     def self.words_of_wisdom
       @@markov.generate_2_sentences
